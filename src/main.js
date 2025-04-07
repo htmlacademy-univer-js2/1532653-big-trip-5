3bc09843181ac7824d48ca5264b813ac46b1d8a6
venfar <farlenkovveniamin@gmail.com>
2025-04-07 20:11:56 +0500

    Шаблонизирует данные фильтров

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import FilterView from './view/filter-view.js';
 import {render} from './framework/render.js';
 import TripEventsPresenter from './presenter/trip-events-presenter.js';
 import PointsModel from './model/points-model.js';
+import {generateFilter} from './mock/filter.js';
 
 const siteContentElement = document.querySelector('.trip-events');
 const siteFiltersElement = document.querySelector('.trip-controls__filters');
@@ -10,7 +11,8 @@ const tripEventsPresenter = new TripEventsPresenter({
   tripEventsContainer: siteContentElement,
   pointsModel,
 });
+const filters = generateFilter(pointsModel.points);
 
-render(new FilterView(), siteFiltersElement);
+render(new FilterView({filters}), siteFiltersElement);
 
 tripEventsPresenter.init();
